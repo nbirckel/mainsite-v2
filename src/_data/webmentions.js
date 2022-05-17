68b05eda91ada041080d458e07a6d54739298b1a
Nicolas Birckel <nbirckel@users.noreply.github.com>
2022-05-17 14:42:39 +0200

    Add files via upload

--- src/_data/webmentions.js
+++ src/_data/webmentions.js
@@ -2,7 +2,7 @@
 const WEBMENTION_BASE_URL = "https://webmention.io/api/mentions.jf2";
 
 module.exports = async () => {
-  const domain = "www.nicolas-birckel.fr;"" // e.g. lukeb.co.uk
+  const domain = "www.nicolas-birckel.fr;" // e.g. lukeb.co.uk
   const token = "-a-Yj8YaN2kG6-zBnmlTmQ"; // found at the bottom of https://webmention.io/settings
 
   const url = `${WEBMENTION_BASE_URL}?domain=${domain}&token=${token}&per-page=1000`;
